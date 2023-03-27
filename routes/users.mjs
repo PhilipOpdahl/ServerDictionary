131e194de3e27af78a6a3b8eecba8ca6156ccaeb
PhilipOpdahl <Philip.opdahl@gmail.com>
2023-03-27 21:53:26 +0200

    users saved in pg

--- routes/users.mjs
+++ routes/users.mjs
@@ -1,39 +1,10 @@
-import express from 'express'
-
-const userRoute = express.Router();
-let users = [];
-
-userRoute.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-    const foundUser = users.find(user => user.username === username);
-  
-    if (foundUser && foundUser.password === password) {
-      res.status(200).send({ message: 'Logged in successfully' });
-    } else {
-      res.status(401).send({ message: 'Invalid username or password' });
-    }
-  });  
-
-userRoute.post('/register', async (req, res) => {
-    const newUser = {
-      username: req.body.username,
-      password: req.body.password,
-    };
-  
-    users.push(newUser);
-    res.send(users);
-  });
-
-
-export default userRoute;
-
-/*import express from 'express'
-import * as pg from 'pg'
+import express from 'express';
+import * as pg from 'pg';
 const { Client } = pg.default;
 const { createHmac } = await import('node:crypto');
 const userRoute = express.Router();
 
-const db = process.env.DATABASE_URL;
+const db = process.env.DATABASE_URL || "postgres://mmogxwglkhfwih:28390813195248e339e653ea037ee32f117537fbcd353ac4d745517f28630be1@ec2-54-155-46-64.eu-west-1.compute.amazonaws.com:5432/de85303vmvhnl";
 const credentials = {
     connectionString: db,
     ssl: {
@@ -41,40 +12,13 @@ const credentials = {
     }
 };
 
-userRoute.post('/register', async (req, res) => {
-    console.log('Register request body:', req.body);
-    const client = new Client(credentials);
-    let results = null;
-    try {
-        const hash = createHmac('sha256', req.body.password).digest('hex');
-        await client.connect();
-
-        const query = 'INSERT INTO users("username", "password") VALUES ($1, $2)';
-        const values = [req.body.username, hash];
-        results = await client.query(query, values);
-
-        if (results.rowCount > 0) {
-            res.send({ "Registration": "OK!" });
-        } else {
-            res.status(400).send({ "Error" : "Failed to register user"});
-        }
-
-    } catch (error) {
-        console.error('Error in /register:', error.message);
-        res.status(500).json({ error: 'Internal Server Error' });
-    } finally {
-        await client.end();
-    }
-});
-
 userRoute.post('/login', async (req, res) => {
-    console.log('Login request body:', req.body);
     const client = new Client(credentials);
     try {
         const hash = createHmac('sha256', req.body.password).digest('hex');
         await client.connect();
 
-        const query = 'SELECT * FROM users WHERE username = $1 AND password = $2';
+        const query = 'SELECT * FROM users WHERE usernames = $1 AND password = $2';
         const values = [req.body.username, hash];
         const result = await client.query(query, values);
 
@@ -92,4 +36,29 @@ userRoute.post('/login', async (req, res) => {
     }
 });
 
-export default userRoute; */
+userRoute.post('/register', async (req, res) => {
+    const client = new Client(credentials);
+    let results = null;
+    try {
+        const hash = createHmac('sha256', req.body.password).digest('hex');
+        await client.connect();
+
+        const query = 'INSERT INTO users("usernames", "password") VALUES ($1, $2)';
+        const values = [req.body.username, hash];
+        results = await client.query(query, values);
+
+        if (results.rowCount > 0) {
+            res.send({ "Registration": "OK!" });
+        } else {
+            res.status(400).send({ "Error" : "Failed to register user"});
+        }
+
+    } catch (error) {
+        console.error('Error in /register:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        await client.end();
+    }
+});
+
+export default userRoute;
