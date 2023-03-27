9c865bb93d60122d566df73e116ff29cd747cb70
PhilipOpdahl <Philip.opdahl@gmail.com>
2023-03-27 23:29:15 +0200

    images

--- public/js/Images.mjs
+++ public/js/Images.mjs
@@ -1,42 +1,42 @@
 const movies = [
     {
       Title: 'Star Wars: The Force Awakens',
-      Poster: '/images/Poster1.jpg',
+      Poster: '/Images/Poster1.jpg',
       imdbID: 'tt001',
     },
     {
       Title: '1917',
-      Poster: '/images/Poster2.jpg',
+      Poster: '/Images/Poster2.jpg',
       imdbID: 'tt002',
     },
     {
       Title: 'Joker',
-      Poster: '/images/Poster3.jpg',
+      Poster: '/Images/Poster3.jpg',
       imdbID: 'tt003',
     },
     {
       Title: 'Annette',
-      Poster: '/images/Poster4.jpg',
+      Poster: '/Images/Poster4.jpg',
       imdbID: 'tt004',
     },
     {
       Title: 'Pulp Fiction',
-      Poster: '/images/Poster5.jpg',
+      Poster: '/Images/Poster5.jpg',
       imdbID: 'tt005',
     },
     {
       Title: 'Jurassic Park',
-      Poster: '/images/Poster6.jpeg',
+      Poster: '/Images/Poster6.jpeg',
       imdbID: 'tt006',
     },
     {
       Title: 'Hard Candy',
-      Poster: '/images/Poster7.jpg',
+      Poster: '/Images/Poster7.jpg',
       imdbID: 'tt007',
     },
     {
       Title: 'Matrix',
-      Poster: '/images/Poster8.jpeg',
+      Poster: '/Images/Poster8.jpeg',
       imdbID: 'tt008',
     },
   ];
